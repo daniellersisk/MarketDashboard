7ed0d3e0d72fae08f4af7f94696c9dab17b99a39
Your Name <danielle.sisk@nyu.edu>
2024-11-30 23:52:34 -0800

    adds model migrations

--- marketdashboard/api/models.py
+++ marketdashboard/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Instrument(models.Model):
+    symbol = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    exchange = models.CharField(max_length=15)
+    currency = models.CharField(max_length=15)
+    sector = models.CharField(max_length=20)
+    country = models.CharField(max_length=30)
+    description = models.TextField()
+    asset_type = models.CharField(max_length=30)
+    website = models.CharField(max_length=50)
