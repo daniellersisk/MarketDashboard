510b004a31296845f028c0c9639968a4372d5a6e
Your Name <danielle.sisk@nyu.edu>
2024-12-02 10:48:29 -0800

    updates imports and migrations

--- marketdashboard/marketdashboard/settings.py
+++ marketdashboard/marketdashboard/settings.py
@@ -31,16 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    # 'api',
+    'marketdashboard',
 ]
 
 MIDDLEWARE = [
+    # 'corsheaders.middleware.CorsMiddleware',  # Add this first
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'marketdashboard.urls'
+ROOT_URLCONF = 'marketdashboard.api.urls'
 
 TEMPLATES = [
     {
